72fe1eb2ae46cff029d03f8ddcd2883e8d0703ee
Maxime Brasley <75072652+maxime-brsl@users.noreply.github.com>
2024-05-22 14:34:21 +0200

    création structure + mise en place spring boot

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO client (nom, prenom, email, adresse, numeroCarteBancaire, telephone)
+VALUES ('Brasley', 'Maxime', 'maxime.brasley@gmail.com' ,'10 rue pierre chalnot', '1234567890123456', '0680702581');
---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS client;
+
+CREATE TABLE client (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    adresse VARCHAR(255) NOT NULL,
+    numeroCarteBancaire VARCHAR(16) NOT NULL,
+    telephone VARCHAR(10) NOT NULL
+);
